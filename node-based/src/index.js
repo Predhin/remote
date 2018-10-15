035895d0c9fc4030232e6731016ff664ba2f5fc2
Predhin Tom Sapru <predhin@hotmail.com>
2018-10-15 23:17:06 +0530

    node-based: npm start issue fixed

--- node-based/src/index.js
+++ node-based/src/index.js
@@ -1,5 +1,6 @@
 const app = require('express')();
-const http = require('http').Server(app);
+let http = require('http').Server(app);
+http = require('http-shutdown')(http);
 const io = require('socket.io')(http);
 const {
   cv
@@ -22,8 +23,36 @@ function action(img, rawimg) {
 }
 
 function run() {
+  cleanUp();
   setUpServer();
   startCamera(action);
+}
+
+function cleanUp() {
+  process.stdin.resume();//so the program will not close instantly
+
+  function exitHandler(options, exitCode) {
+    if (options.cleanup) console.log('clean');
+    if (exitCode || exitCode === 0) console.log(exitCode);
+    if (options.exit) process.exit();
+    // shutdown the server.
+    http.shutdown(function () {
+      console.log('Everything is cleanly shutdown.');
+    });
+  }
+
+  //do something when app is closing
+  process.on('exit', exitHandler.bind(null, { cleanup: true }));
+
+  //catches ctrl+c event
+  process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+
+  // catches "kill pid" (for example: nodemon restart)
+  process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+  process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
+
+  //catches uncaught exceptions
+  process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
 }
 
 function setUpServer() {
@@ -38,10 +67,12 @@ function setUpServer() {
       console.log('user disconnected');
     });
   });
+
   // create server
   http.listen(2000, function () {
     console.log('listening on localhost:2000');
   });
+
 }
 
 run();
