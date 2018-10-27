86ad72295af5b9a0b7d337e005eedb47d61f64e4
Anup George <angeorge10@gmail.com>
2018-10-27 18:07:20 +0530

    Adding Gest.js and css changes

--- web-based/src/client/scripts/raspberry.js
+++ web-based/src/client/scripts/raspberry.js
@@ -1,5 +1,8 @@
 (function () {
+    let wave = { count: 0, ready: true };
     $("#toggler").change(function () {
+        wave.ready = false;
+        showLoaderModal();
         let options = {
             type: 'PUT',
             url: '/api/control',
@@ -9,6 +12,14 @@
             contentType: "application/json",
             success: function (data, status) {
                 alert(data.value);
+                hideLoaderModal();
+                wave.ready = true;
+            },
+            error: function (req, statusText, errorString) {
+                console.log("Error Status : " + statusText + " - Error String : " + errorString);
+                hideLoaderModal();
+                wave.ready = true;
+
             },
             dataType: 'json'
         }
@@ -41,15 +52,42 @@
             success: function (data, status) {
                 if (data) {
                     if (data.value === "ON") {
-                        $('#toggler').attr('checked', 'checked');
+                        $('#toggler').prop('checked', true);
                     } else {
-                        $('#toggler').removeAttr('checked');
+                        $('#toggler').prop('checked', false);
                     }
                 }
+                hideLoaderModal();
+            },
+            error: function (req, statusText, errorString) {
+                console.log("Error Status : " + statusText + " - Error String : " + errorString);
+                hideLoaderModal();
             }
         };
+        showLoaderModal();
         $.ajax(options);
     }
     readLEDState();
-
+    gest.start();
+    gest.options.subscribeWithCallback(function (gesture) {
+        if (wave.ready) {
+            console.log(gesture);        
+            wave.count++;
+            if (wave.count % 3 === 0) {
+                if ($('#toggler:checked').length > 0) {
+                    $('#toggler').prop('checked', false);
+                    $('#toggler').trigger('change');
+                } else {
+                    $('#toggler').prop('checked', true);
+                    $('#toggler').trigger('change');
+                }
+            }
+        }
+    });
+    function showLoaderModal(){
+       $('.modal').modal('show');
+    }
+    function hideLoaderModal(){
+       $('.modal').modal('hide');
+    }
 })();
