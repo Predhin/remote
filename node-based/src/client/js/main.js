d0fc7612668afc065322481daa196599ac36ebf0
Joseph <388805@cognizant.com>
2018-10-29 16:13:40 +0530

    Redesign

--- node-based/src/client/js/main.js
+++ node-based/src/client/js/main.js
@@ -1,101 +1,17 @@
-;(function () {
-	
-	'use strict';
+var app = angular.module("gestureControl", []); 
+app.controller("myCtrl", function($scope) {
+	var socket = io();
 
-	// var formTab = function() {
+	var regionOfCapture = document.querySelector('#regionOfCapture');
 
-	// 	$('.tab-menu a').on('click', function(event){
-	// 		var $this = $(this),
-	// 			data = $this.data('tab');
-
-	// 		$('.tab-menu li').removeClass('active');
-	// 		$this.closest('li').addClass('active')
-
-	// 		$('.tab .tab-content-inner').removeClass('active');
-	// 		$this.closest('.tab').find('.tab-content-inner[data-content="'+data+'"]').addClass('active');
-
-	// 		event.preventDefault();
-
-	// 	});
-
-	// };
-	var contentWayPoint = function() {
-		var i = 0;
-
-		// $('.gtco-section').waypoint( function( direction ) {
-
-
-			$('.animate-box').waypoint( function( direction ) {
-
-				if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
-					
-					i++;
-
-					$(this.element).addClass('item-animate');
-					setTimeout(function(){
-
-						$('body .animate-box.item-animate').each(function(k){
-							var el = $(this);
-							setTimeout( function () {
-								var effect = el.data('animate-effect');
-								if ( effect === 'fadeIn') {
-									el.addClass('fadeIn animated-fast');
-								} else if ( effect === 'fadeInLeft') {
-									el.addClass('fadeInLeft animated-fast');
-								} else if ( effect === 'fadeInRight') {
-									el.addClass('fadeInRight animated-fast');
-								} else {
-									el.addClass('fadeInUp animated-fast');
-								}
-
-								el.removeClass('item-animate');
-							},  k * 200, 'easeInOutExpo' );
-						});
-						
-					}, 100);
-					
-				}
-
-			} , { offset: '85%' } );
-		// }, { offset: '90%'} );
-	};
-
-
-
-
-	// Loading page
-	var loaderPage = function() {
-		$(".gtco-loader").fadeOut("slow");
-	};
-
-	var counter = function() {
-		$('.js-counter').countTo({
-			 formatter: function (value, options) {
-	      return value.toFixed(options.decimals);
-	    },
-		});
-	};
-
-	var counterWayPoint = function() {
-		if ($('#gtco-counter').length > 0 ) {
-			$('#gtco-counter').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( counter , 400);					
-					$(this.element).addClass('animated');
-				}
-			} , { offset: '90%' } );
-		}
-	};
-
-
-	
-	$(function(){
-		// formTab();
-		contentWayPoint();
-		loaderPage();
-		counterWayPoint();
-	});
-
-
-}());
+    socket.on('image', function (data) {
+        // image.src = 'data:image/png;base64,' + data;
+    });
+    // TODO - for Dev purpose. Remove post development!!
+    socket.on('captured-image', function (data) {
+        regionOfCapture.src = 'data:image/png;base64,' + data;
+    });
+    socket.on('count', function (data) {
+        document.getElementById("text").innerHTML = data;
+    });
+});
