4e8ae0f2f13f3337491919d9c31163399647bce1
DieOnTime <mail.daion.paul@gmail.com>
2018-10-30 15:39:05 +0530

    Adding fan anc scan logic

--- node-based/src/client/js/script.js
+++ node-based/src/client/js/script.js
@@ -3,9 +3,13 @@
     // var image = document.querySelector('#camera');
     // TODO - for Dev purpose. Remove post development!!
     var regionOfCapture = document.querySelector('#regionOfCapture');
+    var scanner = document.getElementById('loader-scanner');
+    var fan = document.getElementById('fan');
+    var scannerStatus = document.getElementById('scannerStatus');
 
-    socket.on('image', function (data) {
-        // image.src = 'data:image/png;base64,' + data;
+    socket.on('startScan', function (data) {
+        scanner.className = "loader-scanner";
+        scannerStatus.className = "scanner-text text-center";
     });
     // TODO - for Dev purpose. Remove post development!!
     socket.on('captured-image', function (data) {
@@ -13,11 +17,8 @@
     });
     socket.on('count', function (data) {
         document.getElementById("text").innerHTML = data;
-        var scanner = document.getElementById('loader-scanner');
-        if(data === "OFF") {            
-            scanner.className = "loader-scanner stop";
-        } else {
-            scanner.className = "loader-scanner";
-        }
+        scanner.className = "loader-scanner stop";
+        scannerStatus.className = "scanner-text text-center invisible";
+        fan.className = "fan fa-spin " + data.toLowerCase();
     });
 })();
