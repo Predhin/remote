d8048d1ae4170402294f6fb4bdcd674dcdddee12
Predhin Tom Sapru <predhin@hotmail.com>
2018-11-03 22:12:43 +0530

    node-based: style updates

--- node-based/src/client/js/script.js
+++ node-based/src/client/js/script.js
@@ -1,23 +1,28 @@
 (function () {
-    var socket = io();
-    // var image = document.querySelector('#camera');
-    // TODO - for Dev purpose. Remove post development!!
-    var regionOfCapture = document.querySelector('#regionOfCapture');
-    var scanner = document.getElementById('scanning-loader');
-    var fan = document.getElementById('fan');
-    var scannerStatus = document.getElementById('scannerStatus');
-    let instructionPanels = $(".instruction");
+    const socket = io();
+    const regionOfCapture = document.querySelector('#regionOfCapture');
+    const scanner = document.getElementById('scanning-loader');
+    const fan = document.getElementById('fan');
+    const scannerStatus = document.getElementById('scannerStatus');
+    const scannerContainer = $("#scanning-container");
+    const instructionPanels = $(".instruction");
+    const detectionContainer = $('#detection');
+    detectionContainer.hide();
 
     socket.on('startScan', function (data) {
         $(scanner).removeClass('stop');
         $(scanner).addClass('start');
-        // scannerStatus.className = "scanner-text text-center";
+        scannerContainer.show();
+        // detectionContainer.hide();
     });
     socket.on('hand', function (data) {
         if(data) {
             instructionPanels.hide();
+            scannerContainer.show();
         } else {
             instructionPanels.show();
+            // detectionContainer.hide();
+            scannerContainer.hide();
         }
     });
     socket.on('captured-image', function (data) {
@@ -27,8 +32,9 @@
         document.getElementById("text").innerHTML = data;
         $(scanner).removeClass('start');
         $(scanner).addClass('stop');
-        // scanner.className = "scan stop";
-        // scannerStatus.className = "scanner-text text-center invisible";
+        scannerContainer.hide();
+        detectionContainer.show();
+        
         fan.className = "fan-leaf fan fa-spin " + data.toLowerCase();
     });
 })();
