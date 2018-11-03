7c770ddb0b91750366f7d35bb35077808c510c92
Predhin Tom Sapru <predhin@hotmail.com>
2018-11-03 20:09:00 +0530

    node-based: adding assets

--- node-based/src/client/js/script.js
+++ node-based/src/client/js/script.js
@@ -3,13 +3,14 @@
     // var image = document.querySelector('#camera');
     // TODO - for Dev purpose. Remove post development!!
     var regionOfCapture = document.querySelector('#regionOfCapture');
-    var scanner = document.getElementById('loader-scanner');
+    var scanner = document.getElementById('scanning-loader');
     var fan = document.getElementById('fan');
     var scannerStatus = document.getElementById('scannerStatus');
 
     socket.on('startScan', function (data) {
-        scanner.className = "loader-scanner";
-        scannerStatus.className = "scanner-text text-center";
+        $(scanner).removeClass('stop');
+        $(scanner).addClass('start');
+        // scannerStatus.className = "scanner-text text-center";
     });
     // TODO - for Dev purpose. Remove post development!!
     socket.on('captured-image', function (data) {
@@ -17,8 +18,10 @@
     });
     socket.on('count', function (data) {
         document.getElementById("text").innerHTML = data;
-        scanner.className = "loader-scanner stop";
-        scannerStatus.className = "scanner-text text-center invisible";
-        fan.className = "fan fa-spin " + data.toLowerCase();
+        $(scanner).removeClass('start');
+        $(scanner).addClass('stop');
+        // scanner.className = "scan stop";
+        // scannerStatus.className = "scanner-text text-center invisible";
+        fan.className = "fan-leaf fan fa-spin " + data.toLowerCase();
     });
 })();
